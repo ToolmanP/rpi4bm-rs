7998efbf76482f54e51ae82460a9a950f14294db
Yiyang Wu <toolmanp@outlook.com>
2023-10-18 00:24:01 +0800

    feat: Update bootstrapping code for baremetal rpi4b
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn main() {
+    loop {
+    }
+}

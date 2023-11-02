b301842027841b2870a556f6b6f7d37c3ef8191f
Yiyang Wu <toolmanp@outlook.com>
2023-11-02 18:49:45 +0800

    feat: update main.rs
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
 #![no_std]
 #![no_main]
 
-extern crate alloc;
-
 mod io;
 mod mm;
 mod panic;
 
-use io::gpio::GPIO;
-use io::periph::*;
-use io::uart::*;
-use mm::allocator::heap_init;
+extern crate alloc;
+
+use io::uart::{uart_init, uart_write};
 
 #[no_mangle]
 pub extern "C" fn main() {
-
-    heap_init();
-
-    let gpio = GPIO::new(GPSET, GPCLR, GPPUD, GPFSEL);
-    let aux = AUX::new(UART_AUX_BASE);
-    let uart = UART::new(gpio, aux);
-
-    for _ in 0..10 {
-        uart.write_text("Hello world!\n");
-    }
-
+    uart_init();
+    uart_write("UART initialization finished!");
+    uart_write("hello world!");
     loop {}
 }

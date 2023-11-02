d4e4caa1768addbd405c8e649875fcdbf66dd2bd
Yiyang Wu <toolmanp@outlook.com>
2023-11-02 18:47:55 +0800

    TODO: fix allocator
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- src/mm/allocator.rs
+++ src/mm/allocator.rs
@@ -1,13 +1,17 @@
 use buddy_system_allocator::LockedHeap;
+
+use crate::io::uart::uart_write;
+
 #[global_allocator]
-pub static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::<32>::new();
+pub static HEAP_ALLOCATOR: LockedHeap<64> = LockedHeap::<64>::new();
 pub static mut HEAP: [u8; 65536] = [0; 65536];
 
-#[inline(always)]
 pub fn heap_init() {
-    unsafe{
-    HEAP_ALLOCATOR
-        .lock()
-        .init(HEAP.as_mut_ptr() as usize, HEAP.len());
+    unsafe {
+        if let Some (mut heap) = HEAP_ALLOCATOR.try_lock(){
+            heap.init(HEAP.as_mut_ptr() as usize, HEAP.len());
+        } else {
+            uart_write("HEAP LOCK FAILED");
+        }
     }
 }

c7343264f33dfd6d8c72c7b1beb4595e23940b3b
Yiyang Wu <toolmanp@outlook.com>
2023-11-09 00:02:12 +0800

    refactor: update main.rs
    Signed-off-by: Yiyang Wu <toolmanp@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,33 @@
 #![no_std]
 #![no_main]
 
-mod io;
 mod mm;
+mod lib;
 mod panic;
+mod periph;
 
 extern crate alloc;
+extern crate spin;
 
-use io::uart::{uart_init, uart_write};
+use crate::periph::{uart_init, uart_write};
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::lib::stdout(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
 
 #[no_mangle]
 pub extern "C" fn main() {
     uart_init();
-    uart_write("UART initialization finished!");
-    uart_write("hello world!");
+    uart_write("UART initialization finished!\n");
+    // FIXME: Really Don't know why the locking fails here. Needs further digging.
+    println!("hello world!");
     loop {}
 }
+
